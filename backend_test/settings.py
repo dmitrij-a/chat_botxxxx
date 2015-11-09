42f53c18fce78206b696f32454235c798ba7b227
dmitrij-a <dagapof@gmail.com>
2015-11-09 16:29:35 +0300

    Refactored chat window

--- backend_test/settings.py
+++ backend_test/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat_bot'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -105,4 +106,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
--- chat_bot/views.py
+++ chat_bot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from urllib import request
 import lxml.html as html
+from datetime import datetime
+from chat_bot.models import Message
 
 # Create your views here.
 
@@ -8,8 +10,8 @@ import lxml.html as html
 def get_title(site):
     response = request.urlopen(site)
     tree = html.document_fromstring(response.read())
-    title = tree.xpath('/html/head/title/text()')[0]
-    return 'заголовок сайта %s --- %s' % (site, title)
+    title = tree.xpath('/html/head/title/text()')[0].strip()
+    return ' >> заголовок сайта %s -- %s' % (site, title)
 
 
 def get_h1(site):
@@ -22,16 +24,23 @@ def chat_page(request):
 
 
 def chat_bot(request):
-    query = request.GET['query']
+    query = request.POST['query']
     if query.startswith('Бот, дай мне заголовок сайта'):
         site = query.split()[-1:][0]
         if not site.startswith('http') and not site.startswith('https'):
             return render(request, 'chat.html', {'response': 'Адрес сайта должен начинаться с http:// или https://'})
+
         header = get_title(query.split()[-1:][0])
-        return render(request, 'chat.html', {'response': header})
+        new_message = datetime.now().strftime('Дата %d.%m.%y %H:%M:%S') + header
+        Message.objects.create(text=new_message)
+        messages = [mes.text for mes in Message.objects.all()]
+        return render(request, 'chat.html', {'response': messages})
 
     if query.startswith('Бот, дай мне H1 с сайта'):
         h1 = get_h1(query.split()[-1:][0])
         return render(request, 'chat.html', {'response': h1})
+
+
+
 
 
